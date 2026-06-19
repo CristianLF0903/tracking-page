840fad8f57e66847b727ec42fde342b4c7a8bf37
Cristian Londoño Franco <69688343+CristianLF0903@users.noreply.github.com>
2026-06-19 11:33:02 -0500

    fix: agregar variable de entorno APP_BASE_URL para la configuración de despliegue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/',
+	base: process.env.APP_BASE_URL || '/',
 	plugins: [react()],
 	server: {
 		proxy: {
